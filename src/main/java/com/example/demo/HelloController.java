87daba4e96bbf99cb145d08d5764705b558367f3
matharu7 <matharu7777@gmail.com>
2026-04-26 10:50:50 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/api/hello")
     public String sayHello() {
-        return "Backend says: Connection Successful!";
+        return "Connection Successful!";
     }
 }
